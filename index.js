0671697fad0895f68f1a217b05ffe07c5123ccf0
anaclette <laclauana@gmail.com>
2020-09-16 20:16:55 -0300

    responsive menu button

---
+++ index.js
@@ -0,0 +1,26 @@
+const responsiveMenu = document.querySelector("#menu-button")
+const menuLinks = document.querySelector(".toolbar-displaymenu")
+const showLinks = document.querySelector(".display-menu")
+const eachLink = document.querySelectorAll(".toolbar-displaymenu a")
+
+responsiveMenu.onclick = () => {
+    showLinks.classList.remove("ocultar")
+    menuLinks.classList.remove("ocultar")
+}
+
+for (let link of eachLink) {
+
+    link.onclick = () => {
+        console.log(eachLink)
+        showLinks.classList.add("ocultar")
+        menuLinks.classList.add("ocultar")
+
+    }
+}
+
+
+
+
+
+
+
