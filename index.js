62fa42047dd54aae8bf5608920fa8f8550df8a52
anaclette <laclauana@gmail.com>
2020-09-19 15:20:19 -0300

    layout proper corrections as my teacher recommended. also added JS functions for menu button and projects links

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const responsiveMenu = document.querySelector("#menu-button")
-const menuLinks = document.querySelector(".toolbar-displaymenu")
-const showLinks = document.querySelector(".display-menu")
-const eachLink = document.querySelectorAll(".toolbar-displaymenu a")
+// ------------------ responsive hamburger menu ----------------
+
+const responsiveMenu = document.querySelector('#menu-button');
+const menuLinks = document.querySelector('.toolbar-displaymenu');
+const showLinks = document.querySelector('.display-menu');
+const eachLink = document.querySelectorAll('.toolbar-displaymenu a');
 
 responsiveMenu.onclick = () => {
-    showLinks.classList.remove("ocultar")
-    menuLinks.classList.remove("ocultar")
-    responsiveMenu.classList.add("ocultar")
+	showLinks.classList.toggle('ocultar');
+	menuLinks.classList.toggle('ocultar');
+};
+
+// ------------------- project buttons -------------------------
+
+const myProjects = document.querySelectorAll('.each-project');
+const projectButtons = document.querySelectorAll('#my-projects-button');
+
+for (let projectButton of projectButtons) {
+	projectButton.onclick = () => {
+		for (let project of myProjects) {
+			if (project.dataset.name === projectButton.dataset.name) {
+				project.classList.remove('hidden');
+			} else if (project.dataset.integrate === projectButton.dataset.integrate) {
+				project.classList.remove('hidden');
+			} else {
+				project.classList.add('hidden');
+			}
+		}
+	};
 }
-
-for (let link of eachLink) {
-
-    link.onclick = () => {
-        console.log(eachLink)
-        showLinks.classList.add("ocultar")
-        menuLinks.classList.add("ocultar")
-
-    }
-}
-
-
-
-
-
-
-
